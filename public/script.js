f15ce2ec77adcb62af34afe68386ed0a509735e0
Wilfred <wilfredmutwiri20@gmail.com>
2023-10-03 19:18:08 +0300

    add menu functionality

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
-alert("hello there!")
+const menu=document.querySelector("#menu");
+const nav=document.querySelector("#navHolder")
+menu.addEventListener("click",()=>{
+    if(nav.classList.contains("hidden")){
+        nav.classList.remove("hidden");
+    }else{
+        nav.classList.add("hidden")
+    }
+})
